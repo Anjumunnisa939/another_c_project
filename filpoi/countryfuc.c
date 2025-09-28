d7bebb8132cdb3f6fb7acfc5ece6c2cb2d7feb4b
moh-ammad <mohammadsohail9392@gmail.com>
2025-09-28 12:48:48 +0530

    Add today's coding practice: country functions

--- filpoi/countryfuc.c
+++ filpoi/countryfuc.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 void india (){
     printf("india is developing country\n");
-
 return;
 }
 void austrialia(){
     printf("i am in austrialia\n");
-    india();
+    //england();//call england function
+        india();//call india function
 return;
 }
 void england(){
     printf("i am in england\n");
-    austrialia();
+    austrialia();//call austrialia function
+    
+    return;
 
 }
 int main(){
-    england();
+    england();//call england function
     return 0;
 }
